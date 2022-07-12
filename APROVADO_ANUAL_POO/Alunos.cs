c73d3264ae3c324151cdd8bbb39c4d4e6c523b54
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-12 17:32:22 -0300

    primeiro commit

---
+++ APROVADO_ANUAL_POO/Alunos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APROVADO_ANUAL_POO
+{
+    internal class Alunos
+    {
+        public string NomeAluno;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public double PontosFaltantes()
+        {
+            return (60 - NotaFinal());
+        }
+
+    }
+}
---
+++ APROVADO_ANUAL_POO/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace APROVADO_ANUAL_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alunos alunos = new Alunos();
+
+           
+            
+            Console.Write("Nome do aluno: ");
+            alunos.NomeAluno = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            alunos.Nota1 = double.Parse(Console.ReadLine());
+            alunos.Nota2 = double.Parse(Console.ReadLine());
+            alunos.Nota3 = double.Parse(Console.ReadLine());
+
+       
+            Console.WriteLine($"Nota final = {alunos.NotaFinal().ToString("F2",CultureInfo.InvariantCulture)}");
+
+
+            if (alunos.NotaFinal() >= 60)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine($"Faltaram {alunos.PontosFaltantes()} pontos.");
+            }
+
+
+        }
+    }
+}
